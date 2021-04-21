8e22e162852dba9af067ca830043f9752dab3333
yesoer <luisenglaender@web.de>
2021-04-21 14:31:38 +0200

    function structure for piece based move generation

--- src/generator.c
+++ src/generator.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 
+#include "board.h"
 #include "chess.h"
 #include "generator.h"
 
@@ -8,4 +9,41 @@ generate_moves(struct chess* game)
 {
 	// TODO(Aurel): Stub. Fill this with code.
 	assert(("Not implemented yet", 0 != 0));
+}
+
+struct list
+generate_moves_queen(struct PIECE board[], enum POS pos) 
+{
+
+}
+
+struct list
+generate_moves_king(struct PIECE board[], enum POS pos) 
+{
+
+}
+
+
+struct list
+generate_moves_rook(struct PIECE board[], enum POS pos) 
+{
+
+}
+
+struct list
+generate_moves_knight(struct PIECE board[], enum POS pos) 
+{
+
+}
+
+struct list
+generate_moves_pawn(struct PIECE board[], enum POS pos) 
+{
+	
+}
+
+struct list
+generate_moves_bishop(struct PIECE board[], enum POS pos) 
+{
+
 }
