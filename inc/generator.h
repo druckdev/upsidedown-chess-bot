6524072263ae3e6fd558873a18c82fa1e3410a4d
Aurel Weinhold <aurel@weinhold.org>
2021-04-22 13:12:29 +0200

    Pass by reference to avoid slow copying of data.

--- inc/generator.h
+++ inc/generator.h
@@ -1,8 +1,9 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
 
+#include "chess.h"
 #include "types.h"
 
-struct list generate_moves(struct chess* game);
+struct list* generate_moves(struct chess* game);
 
 #endif /* GENERATOR_H */
