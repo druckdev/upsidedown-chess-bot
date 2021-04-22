024537a187e42bc472021db0f984f47dad1e1e68
Aurel Weinhold <aurel@weinhold.org>
2021-04-22 13:14:26 +0200

    Format

--- src/main.c
+++ src/main.c
@@ -8,20 +8,20 @@
 void
 list_example()
 {
-    int ints[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    struct list* list = NULL;
-    for (int i = 0; i < 10; ++i)
-        list = list_push(list, &(ints[i]));
+	int ints[]        = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	struct list* list = NULL;
+	for (int i = 0; i < 10; ++i)
+		list = list_push(list, &(ints[i]));
 
-    for (int i = 0; i < 10; ++i) {
-        printf("%i\n", *(int*)(list->elem));
-        list = list_pop(list);
-    }
+	for (int i = 0; i < 10; ++i) {
+		printf("%i\n", *(int*)(list->elem));
+		list = list_pop(list);
+	}
 }
 
 int
 main(int argc, char* argv[])
 {
 	printf("Upside Down Chess Bot version %'.1f\n", VERSION);
-    //list_example(); // An example on how to use the list.
+	//list_example(); // An example on how to use the list.
 }
