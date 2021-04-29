08088c6546e3021cb9450b331945fd94cc00ba31
druckdev <druckdev@protonmail.com>
2021-04-30 00:28:50 +0200

    Choose always first move

--- src/bot.c
+++ src/bot.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 
 #include "bot.h"
+#include "chess.h"
 #include "types.h"
 
 /*
@@ -15,6 +16,11 @@ evaluate_moves(struct chess* game, struct list moves)
 struct move
 choose_move(struct chess* game, struct list moves_sorted)
 {
-	// TODO(Aurel): Stub. Fill this with code.
-	assert(("Not implemented yet", 0 != 0));
+	struct move* move = (struct move*)moves_sorted.first->object;
+	// TODO: return invalid move
+	// if (move) {
+	return *move;
+	// } else {
+
+	// }
 }
