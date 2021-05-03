d91f5368de8a7d9f2d7455627f6bf1aa0f393d1a
druckdev <druckdev@protonmail.com>
2021-05-03 23:21:30 +0200

    Run benchmarks multiple times and take the avg

--- src/benchmark.c
+++ src/benchmark.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include "board.h"
@@ -6,17 +7,33 @@
 #include "generator.h"
 #include "helper.h"
 
+#define N_FOR_AVG 3
 #define ITERATIONS 10000
+
+static double** cpu_secs;
+static size_t** cpu_nsecs;
+static double** wall_secs;
+static size_t** wall_nsecs;
 
 int
 main(int argc, char* argv[])
 {
 	printf("Benchmarking function generate_moves()...\n");
 
-	// TODO(Aurel): Run multiple times and take average.
+	size_t len = sizeof(test_boards) / sizeof(*test_boards);
 
-	for (size_t i = 0; i < sizeof(test_boards) / sizeof(*test_boards); ++i) {
+	cpu_secs   = malloc(len * sizeof(*cpu_secs));
+	cpu_nsecs  = malloc(len * sizeof(*cpu_nsecs));
+	wall_secs  = malloc(len * sizeof(*wall_secs));
+	wall_nsecs = malloc(len * sizeof(*wall_nsecs));
+
+	for (size_t i = 0; i < len; ++i) {
 		printf("BENCHMARK: %s \n", test_boards[i].fen);
+		// Reserve for average too
+		cpu_secs[i]   = calloc((N_FOR_AVG + 1), sizeof(**cpu_secs));
+		cpu_nsecs[i]  = calloc((N_FOR_AVG + 1), sizeof(**cpu_nsecs));
+		wall_secs[i]  = calloc((N_FOR_AVG + 1), sizeof(**wall_secs));
+		wall_nsecs[i] = calloc((N_FOR_AVG + 1), sizeof(**wall_nsecs));
 
 		// Init game
 		struct chess chess;
@@ -28,35 +45,56 @@ main(int argc, char* argv[])
 		struct list* moves;
 		struct timespec t_start_cpu, t_end_cpu, t_start_wall, t_end_wall;
 
-		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_start_cpu); // CPU time
-		clock_gettime(CLOCK_MONOTONIC, &t_start_wall);         // "actual" time
+		for (size_t j = 0; j < N_FOR_AVG; ++j) {
+			clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_start_cpu); // CPU time
+			clock_gettime(CLOCK_MONOTONIC, &t_start_wall); // "actual" time
 
-		for (size_t j = 0; j < ITERATIONS; ++j) {
-			/* functions to benchmark */
+			for (size_t k = 0; k < ITERATIONS; ++k) {
+				/* functions to benchmark */
 
-			moves = generate_moves(&chess, true);
+				moves = generate_moves(&chess, true);
 
-			/* \functions to benchmark */
+				/* \functions to benchmark */
+			}
+
+			clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_end_cpu);
+			clock_gettime(CLOCK_MONOTONIC, &t_end_wall);
+
+			double cpu_sec = (t_end_cpu.tv_sec - t_start_cpu.tv_sec) +
+							 (t_end_cpu.tv_nsec - t_start_cpu.tv_nsec) * 1e-9;
+			size_t cpu_nsec = (t_end_cpu.tv_sec - t_start_cpu.tv_sec) * 1e9 +
+							  (t_end_cpu.tv_nsec - t_start_cpu.tv_nsec);
+			cpu_secs[i][j]  = cpu_sec;
+			cpu_nsecs[i][j] = cpu_nsec;
+			cpu_secs[i][N_FOR_AVG] += cpu_sec;
+			cpu_nsecs[i][N_FOR_AVG] += cpu_nsec;
+
+			double wall_sec =
+					(t_end_wall.tv_sec - t_start_wall.tv_sec) +
+					(t_end_wall.tv_nsec - t_start_wall.tv_nsec) * 1e-9;
+			size_t wall_nsec = (t_end_wall.tv_sec - t_start_wall.tv_sec) * 1e9 +
+							   (t_end_wall.tv_nsec - t_start_wall.tv_nsec);
+			wall_secs[i][j]  = wall_sec;
+			wall_nsecs[i][j] = wall_nsec;
+			wall_secs[i][N_FOR_AVG] += wall_sec;
+			wall_nsecs[i][N_FOR_AVG] += wall_nsec;
+
+			printf("Generated moves: %li\n", moves->count);
+			printf("Elapsed CPU-time over %i iterations:  %lf sec, %li nsec\n",
+			       ITERATIONS, cpu_sec, cpu_nsec);
+			printf("Elapsed wall-time over %i iterations: %lf sec, %li nsec\n",
+			       ITERATIONS, wall_sec, wall_nsec);
 		}
 
-		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_end_cpu);
-		clock_gettime(CLOCK_MONOTONIC, &t_end_wall);
+		cpu_secs[i][N_FOR_AVG] /= N_FOR_AVG;
+		cpu_nsecs[i][N_FOR_AVG] /= N_FOR_AVG;
+		wall_secs[i][N_FOR_AVG] /= N_FOR_AVG;
+		wall_nsecs[i][N_FOR_AVG] /= N_FOR_AVG;
 
-		double cpu_sec = (t_end_cpu.tv_sec - t_start_cpu.tv_sec) +
-						 (t_end_cpu.tv_nsec - t_start_cpu.tv_nsec) * 1e-9;
-		size_t cpu_nsec = (t_end_cpu.tv_sec - t_start_cpu.tv_sec) * 1e9 +
-						  (t_end_cpu.tv_nsec - t_start_cpu.tv_nsec);
-
-		double wall_sec = (t_end_wall.tv_sec - t_start_wall.tv_sec) +
-						  (t_end_wall.tv_nsec - t_start_wall.tv_nsec) * 1e-9;
-		size_t wall_nsec = (t_end_wall.tv_sec - t_start_wall.tv_sec) * 1e9 +
-						   (t_end_wall.tv_nsec - t_start_wall.tv_nsec);
-
-		printf("Generated moves:\t%li\n", moves->count);
-		printf("Average elapsed CPU-time over %i iterations:  %lf sec, %li nsec\n",
-		       ITERATIONS, cpu_sec, cpu_nsec);
-		printf("Average elapsed wall-time over %i iterations: %lf sec, %li nsec\n",
-		       ITERATIONS, wall_sec, wall_nsec);
+		printf("Average CPU-time (%ix):  %lf sec, %li nsec\n", N_FOR_AVG,
+		       cpu_secs[i][N_FOR_AVG], cpu_nsecs[i][N_FOR_AVG]);
+		printf("Average wall-time (%ix): %lf sec, %li nsec\n", N_FOR_AVG,
+		       wall_secs[i][N_FOR_AVG], wall_nsecs[i][N_FOR_AVG]);
 
 		printf("\n-------------------------------------------\n\n");
 	}
