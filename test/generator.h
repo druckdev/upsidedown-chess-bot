bad1ed22143013957d58a9a03f759fae067e121c
Aurel Weinhold <aurel@weinhold.org>
2021-05-10 14:37:10 +0200

    Add an example unit-test of a private/hidden func
    Also remove unnecessary macro.

---
+++ test/generator.h
@@ -0,0 +1,20 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "chess.h"
+#include "types.h"
+
+// Generate all legal moves for the piece at `pos`.
+struct list* generate_moves_piece(struct PIECE board[], enum POS pos,
+                                  int check_checkless, bool hit_allies);
+
+/**
+ * Generates all moves possible in the current chess-game state.
+ */
+struct list* generate_moves(struct chess* game, int check_checkless,
+                            bool hit_allies);
+
+struct list* generate_moves_piece(struct PIECE board[], enum POS pos,
+		int check_checkless, bool hit_allies);
+
+#endif /* GENERATOR_H */
