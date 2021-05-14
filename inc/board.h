8c66b72dd282cbbc832d5673de980809491ba13a
yesoer <luisenglaender@web.de>
2021-05-14 11:05:47 +0200

    boil down board

--- inc/board.h
+++ inc/board.h
@@ -9,13 +9,6 @@
 
 #define WHITE_TO_BLACK_OFF ('a' - 'A')
 
-bool execute_move(struct PIECE* board, struct move* move);
-char* pos_to_str(enum POS pos, char* str);
-char piece_e_to_chr(enum PIECE_E piece);
-char piece_to_chr(struct PIECE piece);
-struct PIECE chr_to_piece(char fen_piece);
-void fen_to_chess(char* fen, struct chess* game);
-bool* are_attacked(struct list* moves, bool* targets);
-void print_board(struct PIECE board[], struct list* moves);
+struct board* fen_to_bitboard();
 
 #endif /* BOARD_H */
