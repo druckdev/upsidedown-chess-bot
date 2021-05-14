0b2d6cc3e6a5bbe7235df6aff342b00d96a9586f
yesoer <luisenglaender@web.de>
2021-05-14 11:04:48 +0200

    adapt structs etc for bitboards

--- inc/chess.h
+++ inc/chess.h
@@ -9,6 +9,23 @@
 
 #define DEFAULT_BOARD "RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr"
 
+
+enum COLOR { WHITE, BLACK };
+
+// clang-format off
+enum POS {
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A8, B8, C8, D8, E8, F8, G8, H8,
+    MAX
+};
+// clang-format on
+
 enum PIECE_E {
 	EMPTY  = 0,
 	PAWN   = 1,
@@ -20,38 +37,43 @@ enum PIECE_E {
 };
 static int PIECE_VALUES[] = { 0, 1, 4, 4, 5, 9, 1000000 };
 
-/*
- * TODO: These are just stubs to get a general idea of what we need.
- * Change and move them as u wish.
- */
-enum COLOR { WHITE, BLACK };
-// clang-format off
-enum POS {
-    A8, B8, C8, D8, E8, F8, G8, H8,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A1, B1, C1, D1, E1, F1, G1, H1,
-    MAX
-};
-// clang-format on
-
 struct PIECE {
 	enum PIECE_E type;
 	enum COLOR color;
 };
 static struct PIECE empty_piece = { EMPTY, WHITE };
 
+typedef unsigned long long U64;
+
+
+// bitboards to represent an entire board
+// lsb represents A1, msb H8
+struct board {
+	U64 white_pieces;
+	U64 black_pieces;
+	U64 knights;
+	U64 pawns;
+	U64 bishops;
+	U64 kings;
+	U64 queens;
+	U64 rooks;
+};
+
 struct chess {
-	// TODO: We memcpy a lot (when creating new game objects)
-	// This should probably become a real pointer.
-	struct PIECE board[64];
+	struct board board;
 	enum COLOR moving;
 	uint32_t checkmate;
 	int rating;
+};
+
+// holds precomputed move masks for every piece, for every position
+struct move_masks {
+	U64 knights[64];
+	U64 pawns[64];
+	U64 bishops[64];
+	U64 kings[64];
+	U64 queens[64];
+	U64 rooks[64];
 };
 
 struct move {
