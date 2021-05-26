ebabc1be0d3df8125e1b2d9a46a6293ca7f19c8a
druckdev <druckdev@protonmail.com>
2021-05-26 02:32:43 +0200

    Add negamax unit-test and improve memory cleanup
    We are still going to gave memory leaks if one of the asserts in
    `test_negamax` fail.
    This could be solved maybe through a list of pointers for every RUN_TEST
    on which list_free is called afterwards.

--- test/test_bot.c
+++ test/test_bot.c
@@ -25,16 +25,49 @@ test_rate_board()
 			fen_to_chess(fen, &game);
 			rating = rate_board(&game);
 
+			if (rating != c * PIECE_VALUES[p.type] || game.rating != rating)
+				free(game.board);
+
 			TEST_ASSERT_EQUAL_INT(rating, c * PIECE_VALUES[p.type]);
 			TEST_ASSERT_MESSAGE(game.rating == rating,
 			                    "Rating not properly set.");
 		}
 	}
+
+	free(game.board);
 }
 
 void
 test_negamax()
 {
+	struct chess game = { .board  = calloc(64, sizeof(*game.board)),
+		                  .moving = WHITE };
+	fen_to_chess("R1BQKBNR/PPPPPPPP/N7/8/8/8/pppppppp/rnbqkbnr", &game);
+
+	struct negamax_return ret = negamax(&game, 1);
+
+	// clang-format off
+	struct move expected = { .start        = B7,
+	                         .target       = B8,
+	                         .hit          = false,
+	                         .is_checkmate = false,
+	                         .promotes_to.type = QUEEN,
+	                         .promotes_to.color = WHITE
+	};
+
+	// Only works in combination with `__attribute__((packed))` as there will be
+	// random (when allocated with malloc or on stack) padding bytes if the
+	// struct size is not dividable by 4.
+	// TEST_ASSERT_EQUAL_MEMORY(&expected, ret.move, sizeof(expected));
+	TEST_ASSERT_EQUAL_INT(expected.start, ret.move->start);
+	TEST_ASSERT_EQUAL_INT(expected.target, ret.move->target);
+	TEST_ASSERT_EQUAL_CHAR(expected.hit, ret.move->hit);
+	TEST_ASSERT_EQUAL_CHAR(expected.is_checkmate, ret.move->is_checkmate);
+	TEST_ASSERT_EQUAL_INT(expected.promotes_to.type, ret.move->promotes_to.type);
+	TEST_ASSERT_EQUAL_INT(expected.promotes_to.color, ret.move->promotes_to.color);
+	// clang-format on
+
+	free(game.board);
 }
 
 void
