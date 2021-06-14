39b72561787526a2b3f682acf4f97533211fc50f
Aurel Weinhold <aurel@weinhold.org>
2021-06-14 15:39:16 +0200

    Include moving color in hash

--- inc/hashtable.h
+++ inc/hashtable.h
@@ -24,7 +24,7 @@ struct ht_entry {
 struct ht* init_ht(struct ht* ht, size_t size);
 void free_ht(struct ht* ht);
 
-struct ht_entry* ht_update_entry(struct ht* ht, struct piece* board,
+struct ht_entry* ht_update_entry(struct ht* ht, struct piece* board, enum color moving,
 #ifdef DEBUG_NEGAMAX_USE_LIST
                                  struct move_list* moves
 #else  /* DEBUG_NEGAMAX_USE_LIST */
@@ -33,6 +33,6 @@ struct ht_entry* ht_update_entry(struct ht* ht, struct piece* board,
                                  ,
                                  size_t rating, size_t depth);
 
-struct ht_entry* ht_get_entry(struct ht* ht, struct piece* board);
+struct ht_entry* ht_get_entry(struct ht* ht, struct piece* board, enum color moving);
 
 #endif /* HASHTABLE_H */
