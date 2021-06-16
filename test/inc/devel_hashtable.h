eb6ba93daa5900aadad7fba76e8a67256a6de700
Aurel Weinhold <aurel@weinhold.org>
2021-06-16 11:39:51 +0200

    Add one simple test for the hashtable

---
+++ test/inc/devel_hashtable.h
@@ -0,0 +1,43 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "chess.h"
+#include "move.h"
+struct ht_entry {
+	bool used;
+#ifdef DEBUG_NEGAMAX_USE_LIST
+	struct move_list* moves;
+#else  /* DEBUG_NEGAMAX_USE_LIST */
+	struct move* move;
+#endif /* DEBUG_NEGAMAX_USE_LIST */
+	size_t rating;
+	size_t depth;
+	size_t board_hash;
+	struct piece* board;
+};
+
+ssize_t hash_board(size_t size, struct piece* board, enum color moving);
+
+/**
+ * @arg size should always be a power of 2!
+ */
+struct ht* init_ht(struct ht* ht, size_t size);
+void free_ht(struct ht* ht);
+
+struct ht_entry* ht_update_entry(struct ht* ht, struct piece* board,
+                                 enum color moving,
+#ifdef DEBUG_NEGAMAX_USE_LIST
+                                 struct move_list* moves
+#else  /* DEBUG_NEGAMAX_USE_LIST */
+                                 struct move* move
+#endif /* DEBUG_NEGAMAX_USE_LIST */
+                                 ,
+                                 size_t rating, size_t depth);
+
+struct ht_entry* ht_get_entry(struct ht* ht, struct piece* board,
+                              enum color moving);
+
+#endif /* HASHTABLE_H */
