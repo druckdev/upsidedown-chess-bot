896eac52911012ef4ef2085a3eee7f7ff1b8b99a
Aurel Weinhold <aurel@weinhold.org>
2021-06-16 11:42:00 +0200

    Init struct chess using its init function

--- test/test_bot.c
+++ test/test_bot.c
@@ -54,16 +54,8 @@ void
 test_negamax()
 {
 	printf("TEST: negamax\t");
-	struct chess game = {
-		.board       = calloc(64, sizeof(*game.board)),
-		.moving      = WHITE,
-		.trans_table = { 0 },
-	};
-	TEST_ASSERT_NOT_NULL_MESSAGE(init_ht(&game.trans_table,
-	                                     TRANSPOSITION_TABLE_SIZE),
-	                             "init_ht() failed.");
-
-	fen_to_chess("R1BQKBNR/PPPPPPPP/N7/8/8/8/pppppppp/rnbqkbnr", &game);
+	struct chess game = init_chess();
+	fen_to_chess("R1BQKBNR/PPPPPPPP/N7/8/8/8/pppppppp/rnbqkbnr w", &game);
 
 	struct negamax_return ret = negamax(&game, 1, INT_MIN + 1, INT_MAX);
 	struct move* best;
