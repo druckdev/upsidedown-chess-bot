8b7a5ccd0954d52c5967ef9464fccff0d1b95a7b
yesoer <71628012+yesoer@users.noreply.github.com>
2021-06-23 14:27:10 +0200

    implement play game
    (helper methods as stubs)

--- ml_trainer/trainer.py
+++ ml_trainer/trainer.py
@@ -33,6 +33,52 @@ class Trainer:
         # init game
         b_player = processes[0]
         w_player = processes[1] 
+        white_turn = True
+
+        # play game
+        while True: 
+            self.current_move += 1
+            current_player = w_player if white_turn else b_player
+            print("--------------------------------------------------------")
+            print(("White :" if white_turn else "Black :"), current_player)
+
+            # setup input for bot
+            in_str = self.fen_state 
+            in_str += ' w ' if white_turn else ' b ' 
+            in_str += str(self.time_left) 
+            in_str += ' ' + str(self.current_move)
+            state_bytes = bytes(in_str, 'utf-8')
+            print("trigger bot with ", in_str)
+
+            # trigger bot
+            try:
+                current_player.stdin.write(state_bytes)
+                current_player.stdin.flush()
+            
+            except IOError as e:
+                # expected error when bot terminates because they lost
+                if e.errno == errno.EPIPE:
+                    break
+                else:
+                    print(e)
+                    raise
+
+            # get move current player wants to play and execute it
+            for line in iter(current_player.stdout.readline, b''):
+                self.do_move(line)
+                print("new state ", self.fen_state)
+                break
+            
+            white_turn = not white_turn
+
+    #--------------
+    # Helper
+    #--------------
+
+    # executes the move on the classes fen_state and simplified_state
+    def do_move(self, move: str):
+    # updates the classes fen string from it's simplified state
+    def update_fen(self):
 if __name__ == "__main__":
     trainer = Trainer()
     trainer.train()
