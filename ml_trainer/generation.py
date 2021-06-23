0cabe9b2221f40db7f6b393486e7fd246962e577
yesoer <71628012+yesoer@users.noreply.github.com>
2021-06-23 18:42:21 +0200

    implement evolve (untested)

--- ml_trainer/generation.py
+++ ml_trainer/generation.py
@@ -4,9 +4,10 @@ path_to_executable = "../build/bot"
 cmd = [ path_to_executable ]
 
 class Generation:
-    def __init__(self, instances):
+    def __init__(self, instances, keep_from_gen=0.4):
         # setup first generation
         self.processes = []
+        self.keep_from_gen = keep_from_gen
         for i in range(instances):
             p = subprocess.Popen(cmd,
                                  stdin=subprocess.PIPE,
@@ -17,7 +18,6 @@ class Generation:
     #--------------
     # Interface
     #--------------
-
     
     def evolve(self, performances):
         """prepares next gen of processes
@@ -29,11 +29,29 @@ class Generation:
 
         """
         
-        # remove weakest and reproduce strongest (fitness = last performance)
         print("Evolve based on ", performances)
         
-        pass
+        # sort process indexes by performance
+        indexes = range(len(performances))
+        proc_i_by_fitness = sorted(indexes, key=lambda p: performances[p])
+
+        # get strongest
+        n = int(self.keep_from_gen * len(self.processes)) # how many processes to keep
+        strongest = proc_i_by_fitness[:n]
+
+        # reproduce strongest, initalizing the next generation
+        new_processes = strongest
+        for i in range(len(self.processes) - len(strongest)):
+            p = subprocess.Popen(cmd,
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
+            new_processes.append(p)
+        
+        self.processes = new_processes
     
+
     def get_current_gen(self):
         '''Getter for processes'''
         return self.processes
+        
