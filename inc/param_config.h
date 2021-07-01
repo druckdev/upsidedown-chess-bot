3d788d81fe44bb506c6819f2f749f34875f3a865
yesoer <71628012+yesoer@users.noreply.github.com>
2021-07-01 18:12:16 +0200

    add comment

--- inc/param_config.h
+++ inc/param_config.h
@@ -7,6 +7,7 @@ struct config_template {
     int piece_values[7];
 };
 
+// the following is read and written by the ml trainer
 struct config_template config = {3, 0.01, { 0, 100, 400, 400, 500, 900, 1000000 }};
 
 #endif /* HELPER_H */
