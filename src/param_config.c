28c5b751bd6dea17f226cfdb332d70d53e087db6
yesoer <luisenglaender@web.de>
2021-07-02 03:32:47 +0200

    fix build

--- src/param_config.c
+++ src/param_config.c
@@ -1,3 +1,3 @@
 #include "param_config.h"
 // the following is read and written by the ml trainer
-struct config_template config={3,0.01,{0,100,400,400,500,900,1000000}}
+struct config_template config={7.868684231794927,0.0018195730212484434,{0,23,187,728,49,1409,630}};
